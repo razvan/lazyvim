09187fa5f9efd132364061cc901ef73692f367e9
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2023-11-09 21:01:53 +0100

    Enable Rust the plugin

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -9,27 +9,6 @@ return {
     -- add folds to session
     opts = { options = { "folds"} }
   },
-  --    Add quotes, parens , etc around text.
-  --    Usage:
-  --    Old text                    Command         New text
-  ----------------------------------------------------------------------------------
-  --    surr*ound_words             ysiw)           (surround_words)
-  --    *make strings               ys$"            "make strings"
-  --    [delete ar*ound me!]        ds]             delete around me!
-  --    remove <b>HTML t*ags</b>    dst             remove HTML tags
-  --    'change quot*es'            cs'"            "change quotes"
-  --    <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
-  --    delete(functi*on calls)     dsf             function calls
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end
-  },
 }
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
