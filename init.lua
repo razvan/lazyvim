6efabd7b3c0c931d7fc74cea4f2a430203ccf185
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2023-12-01 17:06:33 +0100

    Disable plugin update checker

--- init.lua
+++ init.lua
@@ -6,3 +6,7 @@ vim.cmd.colorscheme("catppuccin")
 vim.api.nvim_set_option("wrap", true)
 vim.api.nvim_set_option("linebreak", true)
 vim.api.nvim_set_option("clipboard", "unnamedplus")
+
+-- To disable format on save for a buffer or globally:
+--   vim.b.autoformat=false
+--   vim.g.autoformat=false
