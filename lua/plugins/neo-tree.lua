d2767b7151562eb46fd3eb58a4ad96d4f9130de6
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-01-01 16:36:14 +0100

    fix(neo-tree): place on the right hand side

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+-- Put Neo Tree on the right hand side.
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  ---@type TSConfig
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {
+    window = {
+      position = "right",
+    },
+  },
+}
